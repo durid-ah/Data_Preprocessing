ec19162d88db1eec08763bc83a1d50709881ebf1
Durid Ahmad <ahmad.durid96@gmail.com>
2019-10-06 12:38:26 -0600

    splitting the origin column into its three categories

--- auto_mpg_regression.py
+++ auto_mpg_regression.py
@@ -7,6 +7,8 @@ mpg_columns = ["mpg", "cylinders", "displacement", "horsepower", "weight", "acce
 
 if response.ok:
     data = response.content.decode('utf8')
+    # stream the data into a pandas dataframe with the regex "\s+" since the values are separated
+    # by multiple spaces rather than the usual commas
     mpg_df = pd.read_csv(io.StringIO(data), names=mpg_columns, sep="\s+")
 
 mpg_data = mpg_df.loc[:, mpg_columns[:-1]]
@@ -19,3 +21,10 @@ print(mpg_data.horsepower.unique())
 mpg_data = mpg_data[mpg_data.horsepower != '?'] # removing unknown information
 
 print('?' in mpg_data.horsepower) # check to see if it worked
+
+# splitting the origin column into categories
+origin = mpg_data.pop('origin')
+mpg_data["USA"] = (origin == 1) * 1.0
+mpg_data["Europe"] = (origin == 2) * 1.0
+mpg_data["Japan"] = (origin == 3) * 1.0
+
